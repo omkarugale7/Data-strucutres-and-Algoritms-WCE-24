669ff45469239ee1e1e7a1336e71dd965bee3aa5
Ravi Jha <73632663+ravikjha7@users.noreply.github.com>
2021-10-11 10:25:54 +0530

    Delete binpow.cpp

--- binpow.cpp
+++
@@ -1,9 +0,0 @@
-long long binpow(long long a, long long b) {
-    if (b == 0)
-        return 1;
-    long long res = binpow(a, b / 2);
-    if (b % 2)
-        return res * res * a;
-    else
-        return res * res;
-}
