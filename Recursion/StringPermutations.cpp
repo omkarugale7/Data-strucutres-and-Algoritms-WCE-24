ef25a0a2d787b6b45443d4d3828d2244d0758bd8
Abhijit Puri <85777554+Abhijitpuri17@users.noreply.github.com>
2021-10-15 00:24:28 +0530

    String Permutations: Formatting

--- Recursion/StringPermutations.cpp
+++ Recursion/StringPermutations.cpp
@@ -3,33 +3,33 @@ using namespace std ;
 
 string ans = "" ;
 void perm(string s,bool flags[])
+{
+	if (ans.size()==s.size()) 
 	{
-		if (ans.size()==s.size()) {
-			cout << ans << " " ;
-		}
-		
-		/**
-         * 
-		 * 	For each position in ans , each character in string s should be added in ans
-		 * 	after deciding char at current pos in ans, recursively call the fun for next pos for ans
-		 * 	Once all recursive calls after current pos are completed. the char taken for current
-		 * 	pos will again be available for other positions
-         * 
-		 */
-		
-		for (int i = 0 ; i < s.size() ; i++)
-		{
-			if (flags[i] == false)
-			{
-				flags[i] = true ;
-				ans += s[i] ;
-				perm(s, flags);
-				ans.pop_back() ;
-                flags[i] = false ;
-			}
-		}
-			
+		cout << ans << " " ;
 	}
+		
+	/**
+	  * For each position in ans , each character in string s should be added in ans
+          * after deciding char at current pos in ans, recursively call the fun for next pos for ans
+     	  * Once all recursive calls after current pos are completed. the char taken for current
+ 	  * pos will again be available for other positions
+       	  * 
+ 	  */
+		
+	for (int i = 0 ; i < s.size() ; i++)
+	{
+	   if (flags[i] == false)
+	   {
+		flags[i] = true ;
+		ans += s[i] ;
+		perm(s, flags);
+		ans.pop_back() ;
+                flags[i] = false ;
+	   }
+	}
+			
+}
 
 int main()
 {
