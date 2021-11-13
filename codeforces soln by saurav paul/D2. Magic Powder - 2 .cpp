571b90625f2db8168f93c133a28d168a71fdef59
Saurabh <patilsaurabh7777777@gmail.com>
2021-11-13 10:55:00 +0530

    codeforces

---
+++ codeforces soln by saurav paul/D2. Magic Powder - 2 .cpp
@@ -0,0 +1,71 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long int ;
+
+class TC{
+
+    public :
+    void solve(){
+        ll n , k ;
+        cin >> n >> k ;
+
+        vector<ll> cost(n) , total(n) ;
+        for(auto &x : cost){
+            cin >> x ;
+        }
+        for(auto &x : total){
+            cin >> x ;
+        }
+        ll low = 0 , high = 1e10   ;
+        ll ans = -1 ;
+
+        while(low <= high){
+
+            ll mid = (low + high) / 2 ;
+            bool posisble = true ;
+
+            ll ase = k ;
+
+            for(int i = 0 ; i < n; i++){
+                ll lagbe = cost[i] * mid ;
+
+                if(lagbe > total[i]){
+                    ase -= (lagbe-total[i]) ;
+                }
+
+                if(ase < 0){
+                    posisble = false ;
+                    break; 
+                }
+            }
+
+            if(posisble){
+                ans = mid ;
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+
+        }
+
+        cout << ans << endl ;
+
+
+    }
+};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    int testcases = 1;
+    for(int i = 1 ; i <= testcases ; i ++){
+        TC tc;
+        tc.solve() ;
+    }
+
+    return 0 ;
+}
---
+++ codeforces soln by saurav paul/E - Sage's Birthday (hard version) .cpp
@@ -0,0 +1,56 @@
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class TC{
+
+    int N ;
+    vector<int> ice_spheres;
+    public :
+    void solve(){
+
+        cin >> N ;
+
+        ice_spheres.assign(N,0) ;
+        vector<int> v(N) ;
+
+        for(int &x : v){
+            cin >> x;
+        }
+
+        sort(v.begin(),v.end()) ;
+
+        int cur = 0 ;
+        for(int i = 1 ; i < N ; i+= 2){
+            ice_spheres[i] = v[cur++] ;
+        }
+        for(int i = 0 ; i < N ; i+= 2){
+            ice_spheres[i] = v[cur++] ;
+        }
+
+
+        int ans = 0 ;
+
+        for(int i = 1; i < N -1 ; i++){
+            if(ice_spheres[i] < ice_spheres[i-1] && ice_spheres[i] < ice_spheres[i+1]) ++ans ;
+        }
+        cout << ans << endl ;
+        for(auto x : ice_spheres) cout << x << " " ;
+        cout << endl ;
+
+    }
+};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    int testcases = 1;
+    for(int i = 1 ; i <= testcases ; i ++){
+        TC tc;
+        tc.solve() ;
+    }
+
+    return 0 ;
+}
